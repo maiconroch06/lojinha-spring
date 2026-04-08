2c3abf2816e923acbac6cba6e49c1325a5416896
Maicon_Rocha <maiconpablo588@gmail.com>
2026-04-08 09:12:30 -0300

    iniciando projeto de uma loja com spring

---
+++ loja/src/test/java/com/maiconroch06/loja/LojaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maiconroch06.loja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
