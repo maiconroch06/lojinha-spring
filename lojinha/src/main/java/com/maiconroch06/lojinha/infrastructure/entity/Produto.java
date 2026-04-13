c2007afe389a4f494f17b82f9ddcd791fe3744ae
Maicon_Rocha <maiconpablo588@gmail.com>
2026-04-13 08:39:30 -0300

    Create Produto.java

---
+++ lojinha/src/main/java/com/maiconroch06/lojinha/infrastructure/entity/Produto.java
@@ -0,0 +1,29 @@
+package com.maiconroch06.lojinha.infrastructure.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor //aparentemente n esta servindo de nada
+@AllArgsConstructor
+@Entity
+@Table(name = "produtos")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private String descricao;
+	private int quantidade;
+	private double preco;
+	
+}
